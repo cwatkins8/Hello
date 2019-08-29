0803cf0fa0de23ccfd383a52dd4a75e7cd3b549f
cwatkins8 <cwatkins8@huskers.unl.edu>
2019-08-29 08:43:43 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cristian Watkins
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cristian Watkins\n");
+
+  return 0;
+}
